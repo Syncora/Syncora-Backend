ff9fc02e430ea49fde31c9bc598b522716889f04
Sh1tters <justinbestworldwide@gmail.com>
2023-06-17 21:07:49 +0200

    Update tables\setup sql profiles

--- src/database/tables/setup.js
+++ src/database/tables/setup.js
@@ -18,16 +18,18 @@ const createTables = (connection) => {
             name: 'guest_profiles',
             definition: `
                 id INT AUTO_INCREMENT PRIMARY KEY,
+                uuid VARCHAR(36) NOT NULL,
                 type VARCHAR(255) NOT NULL,
-                username VARCHAR(255) NOT NULL
+                username VARCHAR(255) NOT NULL UNIQUE
             `
         },
         {
             name: 'user_profiles',
             definition: `
                 id INT AUTO_INCREMENT PRIMARY KEY,
+                uuid VARCHAR(36) NOT NULL,
                 type VARCHAR(255) NOT NULL,
-                username VARCHAR(255) NOT NULL,
+                username VARCHAR(255) NOT NULL UNIQUE,
                 password VARCHAR(255) NOT NULL,
                 createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
