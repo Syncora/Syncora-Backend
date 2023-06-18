332b26b1b82aab3426094abdfb6d25cb3fbf3adc
Sh1tters <justinbestworldwide@gmail.com>
2023-06-18 10:52:10 +0200

    Update user.js to handle more functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const { query } = require('../database/utils/postQuery');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 class User {
     constructor(userData) {
@@ -6,6 +8,48 @@ class User {
         this.type = userData.type;
         this.username = userData.username;
         this.password = userData.password;
+    }
+
+    static async validateUsernameInput(username) {
+        if (!username || username.length < 2) {
+            throw { statusCode: 500, message: 'Invalid username. Username must be at least 2 characters long.' };
+        }
+    }
+
+    static async validatePasswordInput(password) {
+        if (!password) {
+            throw { statusCode: 500, message: 'Invalid password. Password must not be undefined.' };
+        }
+    }
+
+    static async findByUsername(username) {
+        const queryStr = 'SELECT * FROM user_profiles WHERE username = ? LIMIT 1';
+        const values = [username];
+
+        try {
+            const results = await query(queryStr, values);
+            if (results.length === 0) {
+                return null; // User not found
+            }
+
+            const userData = results[0];
+            return new User(userData);
+
+        } catch (error) {
+            throw { statusCode: 500, message: 'An error occurred while fetching the user from the database.' };
+        }
+    }
+
+    async verifyPassword(plainPassword) {
+        try {
+            const hashKey = process.env.HASH_KEY;
+            const stringToBeHashed = plainPassword + hashKey;
+
+            return await bcrypt.compare(stringToBeHashed, this.password);
+
+        } catch (error) {
+            throw { statusCode: 500, message: 'An error occurred while verifying the password.' };
+        }
     }
 
     async register() {
@@ -18,9 +62,9 @@ class User {
             return;
         } catch (error) {
             if (error.code === 'ER_DUP_ENTRY') {
-                throw { statusCode: 409, message: 'The provided username is already in use.' };
+                throw { statusCode: 409, message: error.sqlMessage };
             } else {
-                throw { statusCode: 500, message: 'An error occurred while registering the user.' };
+                throw { statusCode: 500, message: error.sqlMessage };
             }
         }
     }
