085a5bd241ac935b7c2c845f5402c86be8447215
Sh1tters <justinbestworldwide@gmail.com>
2023-06-21 20:18:11 +0200

    Updated `verifyToken` for better validation checks on token payload

--- src/utils/jwtHelper.js
+++ src/utils/jwtHelper.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_KEY;
 
+const config = require('../config/tokenConfig.json')
+const User = require('../models/userModel');
+
 if (!secretKey || secretKey === undefined) {
     throw new Error('JWT secret key not found!');
 };
@@ -15,20 +18,32 @@ function generateToken(data) {
         sub: data.uuid,
         name: data.username,
         aud: data.type,
-        iss: 'Syncora',
+        iss: config.issuer,
         iat: Date.now(),
     };
 
     return jwt.sign(tokenPayload, secretKey, tokenOptions);
 };
 
-function verifyToken(token) {
+async function verifyToken(token) {
     try {
-        return jwt.verify(token, secretKey);
+        const decodedToken = jwt.verify(token, secretKey);
+        const sub = decodedToken.sub;
+        const username = decodedToken.name;
+
+        // Verify sub (UUID)
+        await User.findByUUID(sub);
+
+        // Verify name (username)
+        await User.findByUsername(username);
+
+        return decodedToken;
     } catch (error) {
-        return null;
+        throw { statusCode: 401, message: "Invalid token." };
     }
-};
+}
+
+
 
 module.exports = {
     generateToken,
