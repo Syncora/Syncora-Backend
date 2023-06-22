91952ffb8fff53a12e7ce0fd5d9077bd9cd833b4
Sh1tters <justinbestworldwide@gmail.com>
2023-06-22 13:52:44 +0200

    First draft on `/api/trivia`

--- src/routes/triviaRoutes.js
+++ src/routes/triviaRoutes.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+const { v4: uuidv4 } = require('uuid');
+const TriviaGame = require('../models/triviaGameModel');
+
 // POST /api/trivia
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
+    /* 
+        Create a new trivia game
+    */
+
+    const { sub, name, aud } = req.user;
+
+    if (aud !== 'user') {
+        return res.status(403).json({ error: 'Access denied. Guests are not allowed to create a trivia game.' });
+    }
+
+    // Generate a unqiue ID for the trivia game
+    const id = uuidv4();
+
+    // Extract the required data from the request body
+    const { title, category, questions } = req.body;
+
+    // Create an array of question objects using map
+    const triviaQuestions = questions.map(({ id, timeLimit, question, options, correctAnswer }) => ({
+        id,
+        timeLimit,
+        question,
+        options,
+        correctAnswer
+    }));
+
+    // Create a new instance of the TriviaGame class
+    const triviaGame = new TriviaGame({
+        id,
+        author: sub,
+        title,
+        category,
+        questions: triviaQuestions
+    });
+
+    // Use the created triviaGame instance as needed
+    console.log(triviaGame);
+
     res.status(200).send();
 })
 
