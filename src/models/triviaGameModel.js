0548466c38e90f58473f3e9c5e09157e71bb6e50
Sh1tters <justinbestworldwide@gmail.com>
2023-06-22 20:59:28 +0200

    Created `getAll()` and `getAllByUserId()`

--- src/models/triviaGameModel.js
+++ src/models/triviaGameModel.js
@@ -9,12 +9,38 @@ class TriviaGame {
         this.questions = this.mapQuestions(triviaGameData.questions);
     }
 
-    async save() {
-        try {
-            const databaseName = process.env.SQL_TRIVIA_GAMES_DB_NAME;
-            const queryStr = 'INSERT INTO game_data (game_id, user_id, game_title, category, questions) VALUES (?, ?, ?, ?, ?)';
-            const values = [this.id, this.author, this.title, this.category, JSON.stringify(this.questions)];
+    static async getAllByUserId(userId) {
+        const databaseName = process.env.SQL_TRIVIA_GAMES_DB_NAME;
+        const queryStr = 'SELECT * FROM game_data WHERE user_id = ?';
+        const values = [userId];
 
+        try {
+            const results = await query(databaseName, queryStr, values);
+            return results;
+        } catch (error) {
+            throw new Error(`Failed to retrieve trivia games for user: ${error.message}`);
+        }
+    }
+
+    static async getAll() {
+        const databaseName = process.env.SQL_TRIVIA_GAMES_DB_NAME;
+        const queryStr = 'SELECT * FROM game_data';
+
+        try {
+            const results = await query(databaseName, queryStr);
+            return results;
+        } catch (error) {
+            throw new Error(`Failed to retrieve trivia games for user: ${error.message}`);
+        }
+    }
+
+
+    async save() {
+        const databaseName = process.env.SQL_TRIVIA_GAMES_DB_NAME;
+        const queryStr = 'INSERT INTO game_data (game_id, user_id, game_title, category, questions) VALUES (?, ?, ?, ?, ?)';
+        const values = [this.id, this.author, this.title, this.category, JSON.stringify(this.questions)];
+
+        try {
             await query(databaseName, queryStr, values);
 
             console.log('Trivia game saved successfully');
