95c590665fd8f0b153707664c8fb74b6c4c5a779
Sh1tters <justinbestworldwide@gmail.com>
2023-06-24 20:04:02 +0200

    Fixed bug with integer validation

--- src/utils/validationHelper.js
+++ src/utils/validationHelper.js
@@ -1,12 +1,16 @@
 function isBooleanString(value) {
+    if (!value || value == undefined) return false;
     return value.toLowerCase() === 'true' || value.toLowerCase() === 'false';
 }
 
 function isValidInteger(value) {
-    return Number.isInteger(parseInt(value));
+    if (!value || value === undefined) return false;
+    const parsedValue = parseInt(value, 10);
+    return String(parsedValue) === String(value);
 }
 
 function isListEmpty(list) {
+    if (!list || list == undefined) return false;
     return true ? list.length === 0 : false;
 }
 
